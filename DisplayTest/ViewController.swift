f3610c94088a57e0d78323ac4755fcf7a1093e13
Sua Le <wxlesua@gmail.com>
2016-10-21 18:10:49 +0700

    Initial Commit

---
+++ DisplayTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DisplayTest
+//
+//  Created by Sua Le on 10/21/16.
+//  Copyright © 2016 Sua Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
